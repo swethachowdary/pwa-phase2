e1c9dcecc2a62d23c9dc79857a9a87f9ad22164b
Bhavagnyanavudu <bhavagnyanavudu@gmail.com>
2019-02-16 14:18:39 +0530

    hello

---
+++ js1/swregister.js
@@ -0,0 +1,13 @@
+if('serviceWorker' in navigator){
+	navigator.serviceWorker.register('sw.js',{
+		scope:'./'
+
+   })
+   .then(reg=>{
+       console.log("success");
+   })
+   .catch(error=>{
+       console.log("error");
+   });
+
+}
